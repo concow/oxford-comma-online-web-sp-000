19e7554a385ee87655dbc0d8312abe5a9750d692
Connor Cowan <ccowan003@comcast.net>
2019-06-04 22:14:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,9 +6,18 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(' and ')
   elsif array.length >= 3
-      array[-1] = "and #{array[-1]}"        #set last element to include 'and'
+      array[-1] = "and #{array[-1]}"
       array.join(', ')
   else
       nil
   end
+end
+
+def oxford_comma(array)
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")               #set last element to include 'and'. #insert method inserts given value with given index.
+  end
+  array.join(", ")
 end
